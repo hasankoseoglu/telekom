95700e2106803c6196453b2a44902736c4c1bf57
Hasan Köseoğlu <hasankoseoglu@Hasan-Mac-mini.local>
2025-10-08 11:21:08 +0300

    api klasörü eklendi

--- api/index.py
+++ api/index.py
@@ -6,5 +6,4 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-        message = {"message": "Hello from Python on Vercel!"}
-        self.wfile.write(json.dumps(message).encode())
+        self.wfile.write(json.dumps({"message": "Hello from Python on Vercel!"}).encode())
